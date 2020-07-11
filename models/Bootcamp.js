6480e48ae08b20d8a4154512b31bd3fb1f3c7480
ken <ken@kenoehlers.com>
2020-07-10 20:47:17 -0400

    Code before geocoder implemtation

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+
 const BootcampSchema = new mongoose.Schema(
   {
     name: {
@@ -108,13 +110,13 @@ const BootcampSchema = new mongoose.Schema(
   }
 );
 
-/*
 // Create bootcamp slug from the name
 BootcampSchema.pre('save', function (next) {
   this.slug = slugify(this.name, { lower: true });
   next();
 });
 
+/*
 // Geocode & create location field
 BootcampSchema.pre('save', async function (next) {
   const loc = await geocoder.geocode(this.address);
